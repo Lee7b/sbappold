35f11f602801fb8e498550bf7adf083181c2f992
Sam Burkett <sburk377@gmail.com>
2019-08-19 18:51:25 -0500

    fix api

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 // An api endpoint
 app.post('/api/contact', (req,res) => {
-    console.log(req.body.post);
+    console.log(req.body);
 });
 
 // The "catchall" handler: for any request that doesn't
