c036f5a87e5c36cd9a67c061de189d63a33465c4
Sam Burkett <sburk377@gmail.com>
2019-08-19 19:26:22 -0500

    fix api

--- index.js
+++ index.js
@@ -17,36 +17,36 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// An api endpoint
-app.post('/api/contact', (req,res) => {
-    console.log(req.body);
-    const output = `
-        <h3>Contact Details</h3>
-        <ul>
-            <li>Name: ${req.body.name} </li>
-            <li>Email: ${req.body.email} </li>
-    `;
+// // An api endpoint
+// app.post('/api/contact', (req,res) => {
+//     console.log(req.body);
+//     const output = `
+//         <h3>Contact Details</h3>
+//         <ul>
+//             <li>Name: ${req.body.name} </li>
+//             <li>Email: ${req.body.email} </li>
+//     `;
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'sburk377@gmail.com', // generated ethereal user
-            pass: 'samlee#Wyatt12!' // generated ethereal password
-        }
-    });
+//     // create reusable transporter object using the default SMTP transport
+//     let transporter = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         secure: false, // true for 465, false for other ports
+//         auth: {
+//             user: 'sburk377@gmail.com', // generated ethereal user
+//             pass: 'samlee#Wyatt12!' // generated ethereal password
+//         }
+//     });
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"NodeMailer Contact" <foo@example.com>', // sender address
-        to: 'sb@samburkett.com', // list of receivers
-        subject: 'Contact Request', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output // html body
-    });
-});
+//     // send mail with defined transport object
+//     let info = await transporter.sendMail({
+//         from: '"NodeMailer Contact" <foo@example.com>', // sender address
+//         to: 'sb@samburkett.com', // list of receivers
+//         subject: 'Contact Request', // Subject line
+//         text: 'Hello world?', // plain text body
+//         html: output // html body
+//     });
+// });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
