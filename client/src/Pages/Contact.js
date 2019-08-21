f7af61dbf5e6a30546b6c11c182ad8ceb640a516
Sam Burkett <sburk377@gmail.com>
2019-08-20 21:10:42 -0500

    fixed contact

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -7,21 +7,7 @@ class Contact extends React.Component {
     render() {
     return (
             <Layout>
-            <Form />
-              <div className="container">       
-                <MDBCol className="d-flex justify-content-center" style ={{ paddingTop: "20px" }}>
-                <MDBCard style={{ width: "30rem" }}>
-                    <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-                    <MDBCardBody>
-                    <MDBCardTitle>Email</MDBCardTitle>
-                    <MDBCardText>
-                        You can reach me anytime at my email using the below button. Thanks.
-                    </MDBCardText>
-                    <MDBBtn href="mailto:sb@SamBurkett.com">Email</MDBBtn>
-                    </MDBCardBody>
-                </MDBCard>
-                </MDBCol>
-             </div> 
+            <Form /> 
            </Layout>
     );
 }
