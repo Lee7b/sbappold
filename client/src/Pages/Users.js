bce083a34285f141c2dff13fd6323e317f087177
Sam Burkett <sburk377@gmail.com>
2019-08-22 11:06:53 -0500

    api test

--- client/src/Pages/Users.js
+++ client/src/Pages/Users.js
@@ -1,24 +1,55 @@
-import React, { Component } from 'react';
-import Data from "../api/data/users.json";
+import React from 'react';
+//import Data from "../api/data/users.json";
 
-class Users extends Component {
-    render () {
+
+export default class Users extends React.Component {
+    state = {
+        loading: true,
+        person: null,
+    };
+
+    async componentDidMount() {
+        const url = "https://randomuser.me/api/";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({ person: data.results[0], loading: false })
+    }   
+
+    render() {
         return (
             <div>
-                {Data.map((postDetail, index) => {
-                    return (
-                        <div>
-                        <h2>{postDetail.name}</h2>
-                        <h2>{postDetail.email}</h2>
-                        <h2>{postDetail.phone}</h2>
-                        <br />
-                        <br />
-                        </div>
-                     )
-                })}
-
+                {this.state.loading || !this.state.person ? <div>Loading random user. . .</div> 
+                : <div>
+                    <div>First Name: {this.state.person.name.first}</div>
+                    <div>Last Name: {this.state.person.name.last}</div>
+                    <div>Gender: {this.state.person.gender}</div>
+                    <div>State: {this.state.person.location.state}</div>
+                    <div>Email: {this.state.person.email}</div>
+                  </div>}
             </div>
         )
     }
-}
-export default Users;
+};
+
+
+// class Users extends Component {
+//     render () {
+//         return (
+//             <div>
+//                 {Data.map((postDetail, index) => {
+//                     return (
+//                         <div>
+//                         <h2>{postDetail.name}</h2>
+//                         <h2>{postDetail.email}</h2>
+//                         <h2>{postDetail.phone}</h2>
+//                         <br />
+//                         <br />
+//                         </div>
+//                      )
+//                 })}
+
+//             </div>
+//         )
+//     }
+// }
+// export default Users;
