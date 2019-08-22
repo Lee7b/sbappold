ba77e70653d68f667cf07abceb6292384dcecbc8
Sam Burkett <sburk377@gmail.com>
2019-08-22 08:30:13 -0500

    trying new email

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -16,8 +16,9 @@ class Home extends React.Component {
                         <p className="mx-5 mb-5">
                             This website is a personal project of mine that I've been working on when time permits. It's a single page application that it is currently hosted on Heroku. It uses a React front-end 
                             with a node/express back-end. For the design I am using Material Design
-                            bootstrap for React. I cannot say enough good things about it! I'm utilizing a REST API for the contact component to send form data from the front-end to the back-end.
-                            I will add more components and features as time goes on. Including a weather page, an online poker game with chat, secure register and log in capability,
+                            bootstrap for React. I cannot say enough good things about it!
+                            I will add more components and features as time goes on. Including a weather page, 
+                            an online poker game with chat, secure register and log in capability,
                             and more. 
                         </p>
                         <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View project</MDBBtn>
